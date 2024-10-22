013f758f9741e2128afd33c684675e5c256569f3
Jocelyn3288 <jocelynwilson@westernwyoming.edu>
2024-10-21 23:20:26 -0600

    Add files via upload

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,23 @@
 #
-# Name
-# Date
+# Jocelyn Wilson
+# 10/14/24
 # File Display Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+def main():
+    # Constants
+    contents=''
+
+    # Open the file
+    inFile=open('numbers.txt' , 'r')
+
+    # Read in the data and store in contents
+    contents=inFile.read()
+
+    # Display the contents
+    print(contents)
+ 
+# Call the main function
+main()
