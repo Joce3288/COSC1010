0d77775b4e9d76538f3084083d1a6434420c8840
Jocelyn3288 <jocelynwilson@westernwyoming.edu>
2024-10-22 14:54:11 -0600

    Add files via upload

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,35 @@
 #
-# Name
-# Date
+# Jocelyn Wilson
+# 10/14/24
 # Average of Numbers Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+def main():
+    # constants
+    infile=''
+    
+    # Variables
+    fileNumbers=float
+    totalAmountAdded=float(0.0)
+    runningTotal=float(0.0)
+    averagedNumbers=float
+
+    # Open the file 
+    infile=open('numbers.txt' , 'r')
+
+    # Read its contents
+    fileNumbers=infile.read()
+    for line in infile:
+        number= float(line)
+        totalAmountAdded=totalAmountAdded+line
+        runningTotal=runningTotal+1
+    
+    # Divide for the average
+    averagedNumbers=totalAmountAdded%runningTotal
+
+    print('The average is: ' , averagedNumbers)
+        
+
+main()
