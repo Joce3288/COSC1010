a7f11d5536f9bd42c8bfbb72fe0c2b80bd9e419a
Jocelyn3288 <jocelynwilson@westernwyoming.edu>
2024-10-28 23:13:53 -0600

    Add files via upload

--- Exception-Handling/main.py
+++ Exception-Handling/main.py
@@ -1,7 +1,40 @@
 #
-# Name
-# Date
+# Jocelyn Wilson
+# 10/22/24
 # Exception Handling Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+def main():
+    # Declare variables
+    total=0.0
+    number=0.0
+    counter=0
+
+    # Open file numbers.txt to read
+    try :
+        infile=open('numbers.txt' , 'r')
+    except :
+        print('A IOError has occured trying to read the file')
+
+    try :
+        for line in infile:
+            counter=counter+1
+            number=float(line)
+            print(number)
+            total+=number
+    except :
+        print('A ValueError has occured trying to convert file contents to float numbers')
+    
+    # Close the file
+    infile.close()
+
+    # Calculate the average
+    average=total/counter
+
+    # Display the average of the numbers in the file
+    print('The average is: ' , average)
+
+# Call main()
+main()
--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -1,7 +1,24 @@
 #
-# Name
-# Date
+# Jocelyn Wilson
+# 10/22/24
 # Lottery Number Generator Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+import random 
+
+def main():
+
+    # Make list
+    numbers=[0]*7
+    
+    # Generate seven random numbers assign a random integer
+    for index in range(7):
+            numbers[index]=random.randint(0, 9)
+    
+    # Get just the numbers from the list numbers
+    print('The lottery number is: ' , numbers[:] )
+    
+# Call the main() function
+main()
