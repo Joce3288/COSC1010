80f7bbe24ca9c06b3738fc8732a3987c02ce151d
Jocelyn3288 <jocelynwilson@westernwyoming.edu>
2024-11-04 22:37:10 -0700

    Add files via upload

--- Magic-8-Ball/main.py
+++ Magic-8-Ball/main.py
@@ -1,7 +1,37 @@
 #
-# Name
-# Date
+# Jocelyn Wilson
+# 10/29/24
 # Magic 8 Ball Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+# Import random module
+import random
+
+#  Define main function
+def main():
+    question='' # Is there a question to answer
+    answer='' # Answer to the question asked picked at random
+    responses=[] # list of all the responses from the file 
+
+    # Open file of responses
+    responses=list(open('8_ball_responses.txt' , 'r'))
+
+    # Initialize loop with the first question
+    question=input('Do you have a question? ')
+
+    # While loop to ask questions until they don't have anymore
+    while question == 'y' or 'yes':
+        
+        # Ask them their first question
+        input('What is your question? ')
+        
+        # Display answer to question
+        print(random.choice(responses))
+        
+        # Ask if they have another question to either restart  or end the loop
+        question= input('Do you have another question? ')
+
+# Call the main function
+main()
