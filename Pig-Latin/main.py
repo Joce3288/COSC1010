7f29b3ec7557e9612cd8ae7dacbc0e4f36195a33
Jocelyn3288 <jocelynwilson@westernwyoming.edu>
2024-11-11 21:56:49 -0700

    Add files via upload

--- Pig-Latin/main.py
+++ Pig-Latin/main.py
@@ -1,7 +1,30 @@
 #
-# Name
-# Date
+# Jocelyn Wilson
+# 11/6/24
 # Pig Latin Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+# Define main function
+def main():
+    sentence='' # Original sentence
+    pigLatin=[] # Sentence after being converted
+    words='' # Each word in sentence seperated
+    newSentence=[] # Each piece of the sentence to put together
+    pigLatinSentence=[] # The new sentence in Pig Latin
+    # Get user's sentence
+    sentence=input('What is your sentence? ')
+
+    # Convert to pig latin
+    words=sentence.split()
+    for word in words:
+        pigLatin=word[1:] + word[0] + 'ay'
+        pigLatinSentence.append(pigLatin)
+    newSentence=" ".join(pigLatinSentence)
+        
+    # Display the new sentence
+    print('Your new sentence is' , newSentence)
+
+# Call the main function
+main()
--- Vowels-and-Consonants/main.py
+++ Vowels-and-Consonants/main.py
@@ -1,7 +1,32 @@
 #
-# Name
-# Date
+# Jocelyn Wilson
+# 11/7/24
 # Vowels and Consonants Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+# Define main function
+def main():
+    mainString='' # String to count
+    vowels='aeiouAEIOU' # what vowels are
+    consonants='bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ' # How many consonants there are
+    numVowels=0 # How many vowels there are
+    numConsonants=0 # How many consonants there are
+    
+    # Get the string to count from
+    mainString=input('What do you want the number of vowels and consonants counted for? ')
+
+    for char in mainString:
+        if char in vowels:
+            numVowels +=1
+    
+    for char in mainString:
+        if char in consonants:
+            numConsonants +=1
+    
+    # Display the number of vowels and consonants
+    print('The number of vowels is' , numVowels , 'and the number of consonants is ', numConsonants )
+
+# Call the main function
+main()
